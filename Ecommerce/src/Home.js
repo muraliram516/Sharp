dcb8667c972db7234b09166ddb53ac18e4f7e67f
Muraliram <115497046+muraliram516@users.noreply.github.com>
2024-02-12 15:28:13 +0530

    Home.js

--- Ecommerce/src/Home.js
+++ Ecommerce/src/Home.js
@@ -2,20 +2,38 @@ import React from 'react';
 import Header from './Header';
 
 const Home = () => {
+  const tourDates = [
+    {
+      date: 'JUL 16',
+      city: 'DETROIT, MI',
+      venue: 'DTE ENERGY MUSIC THEATRE',
+    },
+    {
+      date: 'JUL 19',
+      city: 'TORONTO, ON',
+      venue: 'BUDWEISER STAGE',
+    },
+   
+    {
+      date: 'AUG 7',
+      city: 'CONCORD, CA',
+      venue: 'CONCORD PAVILION',
+    }
+  ];
+
   return (
     <div>
-      <Header />
-      <div className="container mt-4">
-        {/* Main Content */}
-        <h1>The Generics</h1>
-        <p>Get our Latest Album</p>
-        <button>►</button>
-        <h2>TOURS</h2>
-        {/* Tour Dates Table */}
-        {/* Add your tour dates here */}
-      </div>
+      <h1>Tours</h1>
+      {tourDates.map((tour, index) => (
+        <div key={index} className="tour-date">
+          <div className="date">{tour.date}</div>
+          <div className="city">{tour.city}</div>
+          <div className="venue">{tour.venue}</div>
+          <a href={tour.link} target="_blank" rel="noopener noreferrer" className="buy-tickets">BUY TICKETS</a>
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default Home;
